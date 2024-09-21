1714b0e3c2290b535dc11e41b6e3e11f1b30c8ca
aseel-gha <aseel.gh@anvilerp.com>
2024-09-21 18:40:34 +0400

    Work order

---
+++ euw/euw/doctype/dds/test_dds.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Aseel Gh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDDS(FrappeTestCase):
+	pass
---
+++ euw/euw/doctype/nrc/nrc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Aseel Gh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NRC', {
+	// refresh: function(frm) {
+
+	// }
+});
