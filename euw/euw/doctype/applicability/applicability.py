bd2418979d0531cd5a89ad6897bce806d1980bb5
root <root@erpnext-169873-erp-0.erpnext-169873-erp-headless.erpnext-169873.svc.cloudclusters.net>
2024-09-24 08:27:22 +0000

    edit fields

---
+++ euw/euw/doctype/applicability/applicability.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Aseel Gh and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Applicability(Document):
+	pass
